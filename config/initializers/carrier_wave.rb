b14171bdf0f4f201e2405cf62f89966851d0b46a
Thomas C. Pan <thomascpan@gmail.com>
2015-06-05 18:08:33 -0700

    Edited Carrier wave and picture uploader

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,11 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIA3SKGETLEELDFHQ
-'],
-      :aws_secret_access_key => ENV['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
-']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['merp-merp']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
